448e9d2e864eb8932efc9ac49e5f3544e9e35bef
Revit-PC1 <Revit-PC1@PC1>
2022-01-12 17:08:58 +0800

    Добавьте файлы проекта.

---
+++ 19_Task_WPF/Models/Сircumference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_Task_WPF.Models
+{
+    static class Сircumference
+    {
+        public static double Circumf(double a)=>Math.PI*2*a;
+    }
+}
---
+++ 19_Task_WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using _19_Task_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _19_Task_WPF.ViewModels
+{
+    internal class MainWindowViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string PropertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double r;
+        public double R
+        {
+            get { return r; }
+            set { r = value; OnPropertyChanged(); }
+        }
+        private double l;
+        public double L
+        {
+            get { return l; }
+            set { l = value; OnPropertyChanged(); }
+        }
+        public ICommand СircumferenceCommand { get; }
+        private void OnСircumferenceCommandExecuted(object p)
+        {
+            L= Сircumference.Circumf(r);
+
+        }
+        private bool CanСircumferenceCommandExecuted (object p)
+        {
+            if (r!=0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            СircumferenceCommand=new RelayCommand(OnСircumferenceCommandExecuted, CanСircumferenceCommandExecuted);
+        }
+    }
+}
